c60888efc06a5b17b928628abd8dbddc084a8ee7
nimit <garg.nimit36@gmail.com>
2024-06-30 20:57:32 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: 'https://blogspace2.vercel.app/'
+  origin: 'https://blogspace2.vercel.app'
 }));
 require('dotenv').config();
 
